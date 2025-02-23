f8173e01bdb1d3696eeceefe4c67a549f705e927
Sergey <spavelin@inbox.ru>
2025-02-23 17:17:10 +0500

    fix views

--- src/main/java/ru/shaxowskiy/models/User.java
+++ src/main/java/ru/shaxowskiy/models/User.java
@@ -1,6 +1,8 @@
 package ru.shaxowskiy.models;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,8 +20,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "Username should not be empty")
+    @Size(min = 3, max = 30, message = "Login should be between 3 and 30 characters")
     @Column(name = "login")
     private String username;
     @Column(name = "password")
+    @NotEmpty(message = "Password should be not empty")
+    @Size(min = 3, message = "Your password is over short")
     private String password;
 }
