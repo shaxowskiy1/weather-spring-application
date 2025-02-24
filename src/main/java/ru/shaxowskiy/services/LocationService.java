3840de2e0e8c03d54caf79fbe834d73dd6c846f4
Sergey <spavelin@inbox.ru>
2025-02-24 21:45:58 +0500

    fix after checkout

---
+++ src/main/java/ru/shaxowskiy/services/LocationService.java
@@ -0,0 +1,35 @@
+package ru.shaxowskiy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.shaxowskiy.models.Location;
+import ru.shaxowskiy.models.User;
+import ru.shaxowskiy.models.dto.LocationDTO;
+import ru.shaxowskiy.repositories.LocationRepository;
+
+import java.util.List;
+
+@Service
+public class LocationService {
+
+    private LocationRepository locationRepository;
+
+    @Autowired
+    public LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+
+    public void save(LocationDTO locationDTO) {
+        Location location = new Location();
+        location.setName(locationDTO.getName());
+        location.setLatitude(locationDTO.getCoord().getLat());
+        location.setLongitude(locationDTO.getCoord().getLon());
+//        location.setUser(new User());
+        locationRepository.save(location);
+    }
+
+    public List<Location> findAll() {
+        return locationRepository.findAll();
+    }
+}
---
+++ src/main/java/ru/shaxowskiy/services/OpenWeatherApiService.java
@@ -0,0 +1,36 @@
+package ru.shaxowskiy.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.shaxowskiy.models.dto.LocationDTO;
+
+@Service
+public class OpenWeatherApiService {
+
+    @Value("${API_KEY}")
+    private String API_KEY;
+    private final String SEARCH_LOC_OF_NAME = "https://api.openweathermap.org/data/2.5/weather?";
+    private final String SEARCH_LOC_OF_COORD = "https://openweathermap.org/current#one";
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public OpenWeatherApiService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public LocationDTO getInfoByCity(String city) throws JsonProcessingException {
+        String url = SEARCH_LOC_OF_NAME + "q=" + city + "&APPID=" + API_KEY;
+        String forObject = restTemplate.getForObject(url, String.class);
+        LocationDTO locationDTO = objectMapper.readValue(forObject, LocationDTO.class);
+        System.out.println(locationDTO);
+        return locationDTO;
+    }
+}
